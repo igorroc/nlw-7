a5964c77be2976c0a24d0f830050484a7d0e5513
IgorRoc <igor_roc@hotmail.com.br>
2021-10-18 21:58:41 -0300

    [add]: Sending message

--- impulse/src/routes.ts
+++ impulse/src/routes.ts
@@ -1,8 +1,16 @@
 import { Router } from "express"
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController"
+import { CreateMessageController } from "./controllers/CreateMessageController"
+import { EnsureAuthenticated } from "./middleware/EnsureAuthenticated"
 
 const router = Router()
 
 router.post("/authenticate", new AuthenticateUserController().handle)
+
+router.post(
+	"/messages",
+	EnsureAuthenticated,
+	new CreateMessageController().handle
+)
 
 export { router }
